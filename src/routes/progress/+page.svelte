3f4173fc1d20f7b0979fd4697868c0832d7eb449
Long <git.o1dcn@thelong.dev>
2023-11-11 02:09:31 +0100

    Add folder progress

---
+++ src/routes/progress/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>Progress</title>
+	<meta name="description" content="Progress" />
+</svelte:head>
+
+<section>
+	<h2>Progress</h2>
+</section>
+
+<style>
+	section {
+		text-align: center;
+	}
+</style>
