df8c4fe71d8b538cc03ab60b8ffa5375601d2c59
Long <git.o1dcn@thelong.dev>
2023-12-05 10:28:51 +0100

    Fix lighthouse CI
    Add start serve command
    
    Signed-off-by: Long <git.o1dcn@thelong.dev>

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,5 +1,13 @@
 module.exports = {
 	ci: {
+		collect: {
+			url: '0.0.0.0:3000',
+			startServerCommand: 'node build',
+			startServerReadyPattern: 'Listening on 0.0.0.0:3000'
+		},
+		assert: {
+			preset: 'lighthouse:no-pwa'
+		},
 		upload: {
 			target: 'temporary-public-storage'
 		}
